a9094e4e1568bd3309fc34a58d17666f2db43ae5
Alam <alam.15@outlook.com>
2022-03-25 00:15:03 -0400

    add some types

---
+++ src/types/Record.ts
@@ -0,0 +1,11 @@
+export interface Record {
+  id: string;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  email: string;
+  age: string;
+  address: string;
+  foodDelivered: boolean;
+  comment: string;
+}
